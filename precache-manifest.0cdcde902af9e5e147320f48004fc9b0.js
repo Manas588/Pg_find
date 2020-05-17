7edcf6542fa964047bf206bc8c369fb0095b8c3a
Manas Kulkarni <61631964+Manas588@users.noreply.github.com>
2020-05-17 13:13:26 +0530

    Updates

---
+++ precache-manifest.0cdcde902af9e5e147320f48004fc9b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d8c3e56efa230110ae3f0072f68c2d",
+    "url": "/Pg_find/index.html"
+  },
+  {
+    "revision": "50c9d62e3bbed6918d41",
+    "url": "/Pg_find/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "4c7e84d9f74e983a805b",
+    "url": "/Pg_find/static/css/main.00182901.chunk.css"
+  },
+  {
+    "revision": "50c9d62e3bbed6918d41",
+    "url": "/Pg_find/static/js/2.02479801.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/Pg_find/static/js/2.02479801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c7e84d9f74e983a805b",
+    "url": "/Pg_find/static/js/main.aeb164fa.chunk.js"
+  },
+  {
+    "revision": "471816e54c259e179361",
+    "url": "/Pg_find/static/js/runtime-main.490d2cb4.js"
+  },
+  {
+    "revision": "616e20784044e224da037a89c82d6eec",
+    "url": "/Pg_find/static/media/Hyderabad.616e2078.jpg"
+  },
+  {
+    "revision": "730b9edbe8c2c7e427f1d677e006482a",
+    "url": "/Pg_find/static/media/kolkata.730b9edb.jpg"
+  },
+  {
+    "revision": "e46e69ff2b8aa5fe33917d99858f26f5",
+    "url": "/Pg_find/static/media/mumbai.e46e69ff.jpg"
+  },
+  {
+    "revision": "489c74967fa4d1b44bcef95d17cc5f93",
+    "url": "/Pg_find/static/media/pg1.489c7496.jpg"
+  },
+  {
+    "revision": "d6dd3e5c4f2458eec85ebf63fc3c6eb0",
+    "url": "/Pg_find/static/media/pg2.d6dd3e5c.jpg"
+  }
+]);
